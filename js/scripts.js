0062afb3ed2e71b21b4559fd35b6c92210d54357
Cynthia Smith and Rita Bennett-Chew <cynthia@>
2018-06-04 15:06:55 -0700

    add functionality to takes inputs in uppercase and sort alphabetically for icecream flavors

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
-var flavors = ['chocolate', 'peppermint', 'pisatchio', 'vanilla', 'strawberry'];
-flavors.forEach(function(flavor) {
-  //var = favFlavors[]
-  $('ul#cream').append("<li>" + flavor + "</li>");
-});
+
+  //display the flavors using JS
+  var flavors = ['chocolate', 'peppermint', 'pisatchio', 'vanilla', 'strawberry'];
+  flavors.forEach(function(flavor) {
+    $('ul#cream').append("<li>" + flavor + "</li>");
+  });
+
+
+  $("#submit").click(function(e) {
+    e.preventDefault();
+    var inputFlavors = ["ice-cream1", "ice-cream2", "ice-cream3", "ice-cream4"]
+
+    var finalFlavors = [];
+
+    inputFlavors.forEach(function(inputFlavor) {
+      var flavor = $("#" + inputFlavor).val();
+      var upperInput = flavor.toUpperCase();
+      finalFlavors.push(upperInput);
+    });
+    finalFlavors.sort();
+    console.log(finalFlavors);
+    // return finalFlavors.sort();
+  });
 
 });
